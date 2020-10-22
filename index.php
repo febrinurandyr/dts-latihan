382c3607870c8e8a6e70a1f61090c824618b319b
febrinurandyr <73266408+febrinurandyr@users.noreply.github.com>
2020-10-22 12:21:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-    $txt = "Hello world!";
-    $x = 5;
-    $y = 10.5;
-    
-    echo $txt;
-
 require 'includes/config.php';
 require 'includes/functions.php';
 
 init();
-
-?>
 
